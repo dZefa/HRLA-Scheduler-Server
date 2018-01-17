2379710073c5e7d713db72e2aed26223a593a62c
Daniel Chong <danchong625@gmail.com>
2018-01-16 20:55:38 -0800

    [ops] adds foreign key to user table

--- db/model/contact.js
+++ db/model/contact.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 
 const db = require('../index');
+const User = require('./user');
 
 const Contact = db.define('Contact', {
   name: {
@@ -12,5 +13,8 @@ const Contact = db.define('Contact', {
     allowNull: false,
   },
 });
+
+User.hasMany(Contact, { foreignKey: 'userId', allowNull: false, onDelete: 'CASCADE' });
+Contact.belongsTo(User, { foreignKey: 'userId', allowNull: false, onDelete: 'CASCADE' });
 
 module.exports = Contact;
